48bcb0199bd8e6df111fd57cfa3d702035e28274
tnantoka <tnantoka@bornneet.com>
2017-01-18 23:36:55 +0900

    Add gapped spiral

---
+++ _posts/2017-01-18-gapped_spiral.md
@@ -0,0 +1,10 @@
+---
+layout: post
+title: "Gapped Spiral"
+description: "Circle by arc with gap."
+refs:
+  - text: "Processing: A Programming Handbook for Visual Designers and Artists (Second Edition)"
+    href: https://processing.org/handbook/
+---
+
+{% include sketch.html name="gapped_spiral" %}
---
+++ sketches/gapped_spiral.js
@@ -0,0 +1,34 @@
+class Spiral {
+  constructor() {
+    this.gap = random(10, 20)
+    this.thickness = 1
+    this.arcLength = random(0.1, 5.0)
+  }
+
+  display() {
+    clear()
+    stroke(0)
+    noFill()
+    strokeWeight(this.thickness)
+    for (let i = this.gap; i < width; i += this.gap) {
+      const radian = radians(i)
+      arc(width / 2, height / 2, i, i, radian, radian + this.arcLength)
+    }
+  }
+}
+
+var spiral
+
+function setup() {
+  const canvas = createCanvas(650, 250)
+  canvas.parent('gapped_spiral')
+  spiral = new Spiral()
+}
+
+function draw() {
+  spiral.display()
+}
+
+function mousePressed() {
+  spiral = new Spiral()
+}
